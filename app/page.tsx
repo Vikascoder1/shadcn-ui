7861a8eeb60cba6ce259d96111f2362028a6939d
vikas <vikaschaurasiya971@gmail.com>
2024-10-17 09:49:18 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>This is the Home Page</h1>
+      </div>
+    </section>
+  )
+}
